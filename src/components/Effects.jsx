d09eb11fb0445bb06af3526f448ca289d5e23f54
Ryan J. Parker <ryan.jos.parker@gmail.com>
2022-10-10 01:57:09 -0700

    add text and animation

---
+++ src/components/Effects.jsx
@@ -0,0 +1,23 @@
+function Effects() {
+  const introText = [
+    'hi.',
+    'my name is ryan parker.',
+    "i'm absolutely in love with code.",
+    'a few things about me:',
+    "i have a bachelor's in anthropology",
+    "i've trained as an archaeologist",
+    'i have a background in fine art sales',
+    'and i like to combine these skills to create immersive and intuitive user experiences',
+    'i love the thrill of solving a tough problem and ',
+    "i'm a highly motivated team player who",
+    // and i'm a full stack creative developer living in vancouver, washington.",
+    // 'i\'m absolutely in love with code.',
+    // 'you can usually find me writing code, reverse-engineering a cool component, or studying new technologies.',
+    // 'i have the great honor of like to build intuitive and immersive user experiences',
+    // 'if i\'m at my desk, my cat Mouse is napping next to me.',
+  ];
+
+  return <div>Effects</div>;
+}
+
+export default Effects;
